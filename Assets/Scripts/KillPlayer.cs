894ca5ea147f3eb438a5821e002fc1a3718e7614
JoshuaMaher <jmaher300@caledonian.ac.uk>
2023-02-25 20:36:25 +0000

    Game Over Screen

---
+++ Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillPlayer : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            SceneManager.LoadScene("Game Over");
+        }
+    }
+
+}
