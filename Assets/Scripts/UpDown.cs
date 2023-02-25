7a8ac28ba86ddc2cfe3190bc60e9bd1871308d82
JoshuaMaher <jmaher300@caledonian.ac.uk>
2023-02-25 18:33:41 +0000

    New Scene and Enemy

---
+++ Assets/Scripts/UpDown.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpDown : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector2(9.8f * 25f, 0f));
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
